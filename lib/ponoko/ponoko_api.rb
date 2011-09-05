abe2a9efa70f975463a0c865818d8763abe03a3f
Henry Maddocks <henry.maddocks@gmail.com>
2011-09-05 19:04:11 +1200

    Test for escaping url params

--- lib/ponoko/ponoko_api.rb
+++ lib/ponoko/ponoko_api.rb
@@ -25,19 +25,19 @@ module Ponoko
     end
     
     def get_nodes node_key = nil
-      resp = @client.get "nodes/#{node_key}"
+      resp = @client.get "nodes/", "#{node_key}"
       handle_error resp unless resp.code =='200'
       JSON.parse(resp.body)
     end
     
     def get_material_catalogue node_key
-      resp = @client.get "nodes/material-catalog/#{node_key}"
+      resp = @client.get "nodes/material-catalog/", "#{node_key}"
       handle_error resp unless resp.code =='200'
       JSON.parse(resp.body)
     end
     
     def get_orders key = nil
-      resp = @client.get "orders/#{key}"
+      resp = @client.get "orders/", "#{key}"
       handle_error resp unless resp.code =='200'
       JSON.parse(resp.body)
     end
@@ -47,13 +47,13 @@ module Ponoko
         p.collect {|k,v| "#{k}=#{v}"}
       end.flatten.join '&'
 
-      resp = @client.get "orders/shipping_options?#{q}"
+      resp = @client.get "orders/shipping_options?", "#{q}"
       handle_error resp unless resp.code =='200'
       JSON.parse(resp.body)
     end
     
     def get_order_status key
-      resp = @client.get "orders/status/#{key}"
+      resp = @client.get "orders/status/", "#{key}"
       handle_error resp unless resp.code =='200'
       JSON.parse(resp.body)
     end
@@ -61,7 +61,7 @@ module Ponoko
     def step_order key
       raise Ponoko::PonokoAPIError, "Ponoko API Sandbox only" unless @base_uri.host =~ /sandbox/
 
-      resp = @client.get "orders/trigger-next-event/#{key}"
+      resp = @client.get "orders/trigger-next-event/", "#{key}"
       handle_error resp unless resp.code =='200'
       JSON.parse(resp.body)
     end
@@ -71,13 +71,13 @@ module Ponoko
         p.collect {|k,v| "#{k}=#{v}"}
       end.flatten.join '&'
 
-      resp = @client.post 'orders', q
+      resp = @client.post 'orders/', q
       handle_error resp unless resp.code =='200'
       JSON.parse resp.body
     end
 
     def get_products key = nil
-      resp = @client.get "products/#{key}"
+      resp = @client.get "products/", "#{key}"
       handle_error resp unless resp.code =='200'
       JSON.parse(resp.body)
     end
@@ -85,54 +85,58 @@ module Ponoko
     def post_product params
 #       boundary = Digest::MD5.hexdigest(Time.now.to_s)
       boundary = "arandomstringofletters"
-      q = params.to_multipart.collect do |p|
+      query = params.to_multipart.collect do |p|
         '--' + boundary + "\r\n" + p
       end.join('') + "--" + boundary + "--\r\n"
 
-      resp = @client.post "products", q, {'Content-Type' => "multipart/form-data; boundary=#{boundary}"}
+#       resp = @client.post("products/", "query", {"Content-Type" => "multipart/form-data; boundary=#{boundary}"})
+      resp = @client.post("products/", query, nil)
 
       handle_error resp unless resp.code =='200'
       JSON.parse(resp.body)      
     end
     
-    def add_image params
-      boundary = "arandomstringofletters"
-      q = params.to_multipart.collect do |p|
-        '--' + boundary + "\r\n" + p
-      end.join('') + "--" + boundary + "--\r\n"
-
-      resp = @client.post "products/add_image", q, {'Content-Type' => "multipart/form-data; boundary=#{boundary}"}
-
-      handle_error resp unless resp.code =='200'
-      JSON.parse(resp.body)      
-    end
-    
-    def add_assembly_instructions params
-      boundary = "arandomstringofletters"
-      q = params.to_multipart.collect do |p|
-        '--' + boundary + "\r\n" + p
-      end.join('') + "--" + boundary + "--\r\n"
-
-      resp = @client.post "products/add_asembly", q, {'Content-Type' => "multipart/form-data; boundary=#{boundary}"}
-
-      handle_error resp unless resp.code =='200'
-      JSON.parse(resp.body)      
-    end
-    
-    def add_hardware params
-      q = params.to_query.collect do |p|
-        p.collect {|k,v| "#{k}=#{v}"}
-      end.flatten.join '&'
-
-      resp = @client.post "products/add_hardware", q
-
-      handle_error resp unless resp.code =='200'
-      JSON.parse(resp.body)      
-    end
-    
+#     def add_image params
+#       boundary = "arandomstringofletters"
+#       q = params.to_multipart.collect do |p|
+#         '--' + boundary + "\r\n" + p
+#       end.join('') + "--" + boundary + "--\r\n"
+# 
+#       resp = @client.post "products/add_image", q, {'Content-Type' => "multipart/form-data; boundary=#{boundary}"}
+# 
+#       handle_error resp unless resp.code =='200'
+#       JSON.parse(resp.body)      
+#     end
+#     
+#     def add_assembly_instructions params
+#       boundary = "arandomstringofletters"
+#       q = params.to_multipart.collect do |p|
+#         '--' + boundary + "\r\n" + p
+#       end.join('') + "--" + boundary + "--\r\n"
+# 
+#       resp = @client.post "products/add_assembly", q, {'Content-Type' => "multipart/form-data; boundary=#{boundary}"}
+# 
+#       handle_error resp unless resp.code =='200'
+#       JSON.parse(resp.body)      
+#     end
+#     
+#     def add_hardware params
+#       q = params.to_query.collect do |p|
+#         p.collect {|k,v| "#{k}=#{v}"}
+#       end.flatten.join '&'
+# 
+#       resp = @client.post "products/add_hardware", q
+# 
+#       handle_error resp unless resp.code =='200'
+#       JSON.parse(resp.body)      
+#     end
+#     
     def handle_error resp
       error = JSON.parse resp.body
-      raise PonokoAPIError, error["error"]["message"]
+      warn error
+      error
+    rescue
+      raise PonokoAPIError, resp
     end
 
     private :handle_error
@@ -156,13 +160,45 @@ module Ponoko
       @access = OAuth::AccessToken.from_hash(consumer, access_keys)
     end
     
-    def get path
-      @access.get PONOKO_API_PATH + path
+    def get path, params = nil
+      @access.get PONOKO_API_PATH + path + CGI.escape(params)
     end
     
     def post path, params, headers = {}
       @access.post PONOKO_API_PATH + path, params, headers
     end
+    
+    def self.authorize
+      print "Enter consumer key: "
+      consumer_key = $stdin.gets.chomp
+
+      print "Enter consumer secret: "
+      consumer_secret = $stdin.gets.chomp
+      
+      
+      consumer = OAuth::Consumer.new(consumer_key,          consumer_secret,
+                                     site:                  'https://www.ponoko.com/',
+                                     request_token_path:    "/oauth/request_token",
+                                     access_token_path:     "/oauth/access_token",
+                                     authorize_path:        "/oauth/authorize")
+      
+      
+
+      request_token = consumer.get_request_token
+      p "\nGo to this url and click 'Authorize' to get the token:"
+      p request_token.authorize_url
+      print "\nEnter token: "
+      token = $stdin.gets.chomp
+  
+      access_token  = request_token.get_access_token(:oauth_verifier => token)
+  
+      p "\nAuthorization complete! Use the following params to access Ponoko:\n\n"
+      p "consumer_key     = '#{consumer.key}'"
+      p "consumer_secret  = '#{consumer.secret}'"
+      p "access_token     = '#{access_token.token}'"
+      p "access_secret    = '#{access_token.secret}'"
+    end
+
   end
 
 end # module Ponoko
