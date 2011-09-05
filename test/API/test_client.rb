609aecd26227180ba050e2edddc759fc24646af9
Henry Maddocks <henry.maddocks@gmail.com>
2011-09-05 19:04:38 +1200

    Revert "Test for escaping url params"
    This reverts commit abe2a9efa70f975463a0c865818d8763abe03a3f.

--- test/API/test_client.rb
+++ test/API/test_client.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + "/../test_helper")
 
-class Test_Client < MiniTest::Unit::TestCase
+class Test_API_Nodes < MiniTest::Unit::TestCase
   def test_oauth
     skip "Un-skip this test if you have edited the OAuth code"
     ponoko = Ponoko::OAuthAPI.new env:             :production,
@@ -10,31 +10,10 @@ class Test_Client < MiniTest::Unit::TestCase
                                   access_secret:   'keys'
 
     # Just test if we can connect
-    ponoko.get_nodes
-  end
-  
-  def test_error_handling
-#     error = jj {"error"=>{"message"=>"Not Found. Unknown key", "request"=>{"key"=>"bogus_key"}}}
-#   
-#     out = Ponoko::handle_error error
-#     assert out == error
-#     
-#     error = Net::HTTP::Exception.new
-#     out = Ponoko::handle_error error
-#     assert out == error
+    # Don't care about the response so eat the exception
+    assert_raises Ponoko::PonokoAPIError do
+      ponoko.get_nodes
+    end
   end
       
-  def test_escape_params
-    skip "Un-skip this test if you have edited the OAuth code"
-    ponoko = Ponoko::OAuthAPI.new env:             :production,
-                                  consumer_key:    'these', 
-                                  consumer_secret: 'are not',
-                                  access_token:    'real', 
-                                  access_secret:   'keys'
-
-
-    resp = ponoko.get_products "fun/ky[] key"
-    p resp
-  end
-  
 end
