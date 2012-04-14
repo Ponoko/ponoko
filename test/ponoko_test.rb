68a8e6c1c27bdd87b40e2720bc8a3a66d9d22c1a
Henry Maddocks <henry.maddocks@gmail.com>
2012-04-14 13:18:35 +1200

    Adding more API methods to smoke test.

--- test/ponoko_test.rb
+++ test/ponoko_test.rb
@@ -9,19 +9,20 @@ end
 $:.push File.dirname(__FILE__) + '/../lib/'
 require 'ponoko'
 
-hr Ponoko::VERSION
+hr "#{Ponoko::VERSION} (#{Ponoko::API_VERSION})"
 
+hr "Using OAuth"
 Ponoko.api = Ponoko::OAuthAPI.new env:             :sandbox,
                                   consumer_key:    'd2KxglROLzixJ53NAwTRVoAVMsmJZ5X7BxjfS7fc', 
                                   consumer_secret: 'vUk5HwgoGBe95P6zfyNrkW4Mkjojvkw84sd6BXik',
                                   access_token:    'Obq8aJ4O7Y8XCfkB1eFUjRMtRILPIfXiA80WvGnd', 
                                   access_secret:   'hNUeBfLcoz5K72TPRO3nW3Ovw7fcjzqyp84NzT7X'
 
-# Ponoko.api = Ponoko::OAuthAPI.new env:             :test,
-#                                   consumer_key:    'mgaFFoAzbnc7I4pOitGWf9ATECzDElfZB072ugmR', 
-#                                   consumer_secret: 'yivct1T8KciWG2astV5ljPsYTjkEGwNIckOUOikS',
-#                                   access_token:    'OZwmvuSbijjMRRvlFH3vYD7tnZCyLiXVaSmtblHr', 
-#                                   access_secret:   'bOAI6FJx1m8QbhPbAxy19orvCf9y1Kbk5rNinhyg'
+# hr "Using Basic Auth"
+# Ponoko.api = Ponoko::BasicAPI.new :env => :sandbox,
+#                                   :app_key => 'f311cc5bbef182b12ea35dfb143f7edd', 
+#                                   :user_access_key => 'fef1fedc13c20d58233617d276fa2381'
+
 
 
 hr "Nodes"
@@ -64,7 +65,7 @@ design = new_product.designs.first
 design.add_material mc['Plastic']['Acrylic']['Blue']['3.0 mm']['P1']
 design.quantity = 3
 
-new_product.update_design! design
+# new_product.update_design! design
 
 pp new_product.designs
 
@@ -78,22 +79,22 @@ pp new_product.assembly_instructions
 
 hr "Hardware"
 new_product.add_hardware! 'COM-00680', 2
+pp new_product.hardware
 
 hardware = new_product.hardware.first
 hardware.quantity = 1
 new_product.update_hardware! hardware
+pp new_product.hardware
 
 pp new_product
 
-new_product.remove_design_image! 'lamp-1_product_page.jpg'
-new_product.remove_assembly_instructions! 'http://www.instructables.com/id/3D-print-your-minecraft-avatar/'
-new_product.remove_hardware! 'COM-00680'
+# new_product.remove_design_image! 'lamp-1_product_page.jpg'
+# new_product.remove_assembly_instructions! 'http://www.instructables.com/id/3D-print-your-minecraft-avatar/'
+# new_product.remove_hardware! 'COM-00680'
 
-pp new_product
+# pp new_product
 
 new_product.delete!
-
-# exit
 
 hr "Orders"
 orders = Ponoko::Order.get!
@@ -103,22 +104,26 @@ pp Ponoko::Order.get! orders.first.ref unless orders.empty?
 
 address = {"first_name" => "John", "last_name" => "Brown", "address_line_1"=>"27 Dixon Street", "address_line_2"=>"Te Aro", "city"=>"Wellington", "state"=>"na", "zip_or_postal_code"=>"6021", "country"=>"New Zealand", "phone_number" => "045678910"}
 
-order = Ponoko::Order.new 'ref' => 'order_ref97', 'delivery_address' => address, 'shipping_option_code' => 'ups_saver'
+order = Ponoko::Order.new 'ref' => 'order_ref97-' + Time.new.to_s, 'delivery_address' => address
 
 order.add_product products.first
 
+hr "Get the shipping options"
 shipping_options = order.shipping_options!
 
 pp shipping_options
 
-pp order
+order.shipping_option_code = shipping_options.first['code'] 
 
+hr "Send the order"
 order.send!
 
 pp order
 
+hr "Get the order status"
 pp order.status
 
+hr "Step the order"
 Ponoko::Sandbox::step_order order
 
 pp order.status
